5f46a198088959888d9a1721d1d07b360b43c3d8
mayur4242 <khatavkarmayur4242@gmail.com>
2024-09-21 13:28:06 +0530

    R&D server-Eureka

---
+++ bootMSProj01-R-DServer-eureka/src/test/java/com/microservice/BootMsProj01RDServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMsProj01RDServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
